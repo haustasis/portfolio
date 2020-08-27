41063373edbf0cebff00493cf96660cf9547c503
TARISO <toni.ariso@gmail.com>
2020-08-27 04:40:54 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-console.log('Its working');
-
 let theme = localStorage.getItem('theme');
 
-if (theme == null) {
+if (theme === null) {
   setTheme('light');
 } else {
   setTheme(theme);
@@ -10,28 +8,27 @@ if (theme == null) {
 
 let themeDots = document.getElementsByClassName('theme-dot');
 
-for (var i = 0; themeDots.length > i; i++) {
+for (let i = 0; i < themeDots.length; i++) {
   themeDots[i].addEventListener('click', function () {
     let mode = this.dataset.mode;
-    console.log('Option clicked:', mode);
     setTheme(mode);
   });
 }
 
 function setTheme(mode) {
-  if (mode == 'light') {
+  if (mode === 'light') {
     document.getElementById('theme-style').href = 'default.css';
   }
 
-  if (mode == 'blue') {
+  if (mode === 'blue') {
     document.getElementById('theme-style').href = 'blue.css';
   }
 
-  if (mode == 'green') {
+  if (mode === 'green') {
     document.getElementById('theme-style').href = 'green.css';
   }
 
-  if (mode == 'purple') {
+  if (mode === 'purple') {
     document.getElementById('theme-style').href = 'purple.css';
   }
 
